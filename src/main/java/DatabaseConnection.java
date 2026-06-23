ca806b9745d4ba02498462bac51a7a1c578bf58e
Ramprabhu.B <ramprabhu9630@gmail.com>
2026-06-23 19:47:00 +0530

    'insert_1Data'

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement; 
 
 public class DatabaseConnection {
     public static void main(String[] args) {
@@ -11,15 +12,31 @@ public class DatabaseConnection {
 
         // 2. Establish the connection using a try-with-resources statement
         // (This ensures the connection closes automatically when done)
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            
+        try{
+        	Connection connection = DriverManager.getConnection(url, user, password);
             if (connection != null) {
                 System.out.println("Successfully connected to the PostgreSQL server!");
             }
             
+            
+            //Creating statement
+        	Statement statement = connection.createStatement();
+            
+        	//Execute query
+            String sql="insert into students values('Ramparbhu.B',20,'ECE')";
+            
+            int rowAffected = statement.executeUpdate(sql);
+            
+    		if(rowAffected==0) {
+    			System.out.print("Not able to insert the data");
+    		}else {
+    			System.out.print(rowAffected+" Data insert Successfully!!");
+    		}
+    		
+
         } catch (SQLException e) {
             System.out.println("Connection failed!");
             e.printStackTrace();
-        }
+        }  
     }
 }
